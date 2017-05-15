ad16987e0e180b988cf745a92f2a9a877d396967
Jeanmarie <jeanmarie@dynamicalperspectives.com>
2017-05-15 10:50:37 -0400

    new content

---
+++ app/data/friends.js
@@ -0,0 +1,104 @@
+//friend data based on Downton Abbey characters
+var friendsobj = [
+	{
+		name:"Lady Mary Crawley",
+		photo: "https://images.forbes.com/media/2013/07/16/0716_mary-crawley_416x416.jpg"
+		scores:["1","1","1","1","1","1","1","1","1","1"]
+	},
+	{
+		name:"Lady Edith Crawley",
+		photo:"",
+		scores:["2","2","2","2","2","2","2","2","2","2"]
+	},
+	{
+		name:"Lady Sybil Crawley",
+		photo:"",
+		scores:["3","3","3","3","3","3","3","3","3","3"]
+	},
+	{
+		name:"Baxter",
+		photo:"",
+		scores:["4","4","4","4","4","4","4","4","4","4"]
+	},
+	{
+		name:"John Bates",
+		photo:"",
+		scores:["5","5","5","5","5","5","5","5","5","5"]
+	},
+  {
+		name:"Anna Bates",
+		photo:"",
+		scores:["5","5","5","5","5","5","5","5","5","5"]
+	},
+	{
+		name:"Violet Crawley- Dowager Countess of Grantham",
+		photo:"",
+		scores:["1","2","1","2","1","2","1","2","1","2"]
+	},
+	{
+		name:"Lord Robert Grantham",
+		photo:"",
+		scores:["2","1","2","1","2","1","2","1","2","1"]
+	},
+	{
+		name:"Lady Cora Grantham",
+		photo:"",
+		scores:["3","2","1","3","2","1","3","2","1","3"]
+	},
+	{
+		name:"Daisy",
+		photo:"",
+		scores:["2","2","3","1","2","2","1","2","3","2"]
+	},
+	{
+		name:"Mrs. Hughes",
+		photo:"",
+		scores:["5","1","4","5","2","3","2","5","5","5"]
+	},
+
+  {
+		name:"Mrs. Patmore",
+		photo:"",
+		scores:["5","1","4","5","2","3","2","5","5","5"]
+	},
+
+  {
+		name:"Thomas Barrow",
+		photo:"",
+		scores:["5","1","4","5","2","3","2","5","5","5"]
+	},
+  {
+		name:"Isobel Crawley",
+		photo:"",
+		scores:["5","1","4","5","2","3","2","5","5","5"]
+	},
+  {
+		name:"Sarah O'Brien",
+		photo:"",
+		scores:["5","1","4","5","2","3","2","5","5","5"]
+	},
+  {
+		name:"Matthew Crawley",
+		scores:["5","1","4","5","2","3","2","5","5","5"]
+	},{
+		name:"Tom Branson",
+		photo:"",
+		scores:["5","1","4","5","2","3","2","5","5","5"]
+	},{
+		name:"Lord Merton",
+		photo:"",
+		scores:["5","1","4","5","2","3","2","5","5","5"]
+	},
+  {
+		name:"Lady Rosamund Painswick",
+		photo:"",
+		scores:["5","1","4","5","2","3","2","5","5","5"]
+	},
+  {
+		name:"Mrs. Hughes",
+		photo:"",
+		scores:["5","1","4","5","2","3","2","5","5","5"]
+	},
+];
+// make it accessible to other files using require.
+module.exports = friendsobj;
---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,65 @@
+// JM FriendFinder Homework api-routes..js
+// Note to Self: For reference see Hot Restaurant Solution api-routes dated May 13, 2017
+
+// LOAD DATA
+// We are linking our routes to a series of "data" sources.
+// These data sources hold arrays of information on table-data, waitinglist, etc.
+// ===============================================================================
+
+
+
+var friendData 	= require('../data/friends.js');
+
+var path = require('path');
+
+// ===============================================================================
+// ROUTING
+// ===============================================================================
+
+module.exports = function(app){
+  // API GET Requests
+  // Below code handles when users "visit" a page.
+  // In each of the below cases when a user visits a link
+  // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
+  // ---------------------------------------------------------------------------
+
+	app.get('/api/friends', function(req, res){
+		res.json(friendData);
+
+	});
+
+  // API POST Requests
+  // Below code handles when a user submits a form and thus submits data to the server.
+  // In each of the below cases, when a user submits form data (a JSON object)
+  // ...the JSON is pushed to the appropriate JavaScript array
+  // (ex. User fills out a reservation request... this data is then sent to the server...
+  // Then the server saves the data to the tableData array)
+  // ---------------------------------------------------------------------------
+
+	app.post('/api/friends', function(req, res){
+		var closestDifference = 100;
+		var difference = 0;
+		var match;
+
+		friendData.forEach(function(friend) {
+				console.log(friend);
+				console.log(req.body);
+
+
+		difference = eval(friend.scores.map(function (num, index) {
+
+		return Math.abs(num - req.body.scores[index]);
+		}).join('+'));
+
+		if (difference <= closestDifference) {
+		closestDifference = difference;
+		match = friend;
+		}
+	});
+		  res.json(match);
+
+    friendData.push(req.body);
+
+    });
+
+	};
